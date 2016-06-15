f8487f63a15b7d6748b62649856bb34ae0b72dd4
TechGIS <742311374@qq.com>
2016-06-15 14:11:19 +0800

    把引用的js单独分为一个文件夹下jsext

--- modules/istudy/js/istudyMain.js
+++ modules/istudy/js/istudyMain.js
@@ -5,10 +5,15 @@
  */
 define(function(require){
 	var navheader = require("../../../common/js/navheader");
+	var prjUtil = require("../../../common/js/prjUtil");
     navheader.init();
     $('.navbar-nav li a').css('color', '#9d9d9d');
     $('.navbar-nav li:nth(2) a').css('color', '#fff');
 
+    //判断身份
+    var identity = prjUtil.sampleDecode(localStorage.getItem('identity'));
+    // console.log(identity);
+   
     $('.tea-course-set').on('click',function(event) {
     	window.location.href = "istudy-tea.html";
     });
